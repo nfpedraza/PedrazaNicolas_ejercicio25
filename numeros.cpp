861e0b9600b67d94461492c6b90456a70a190aca
nfpedraza <nifepegi@gmail.com>
2019-11-06 15:37:48 +0000

    numeros_corr

--- numeros.cpp
+++ numeros.cpp
@@ -1,40 +1,82 @@
-#include <iostream>
-#include <cmath>
+#include<iostream>
 
+using 
+namespace std;
 
-int N;
-float Mat[0][0];
-int lleno;
+void rellenos(int N,float **b);
 
-float Relleno(){
-    for (int i = 0; i < N+2; i++){
-        for (int j = 0; j < N; j++){
-            lleno= i+j;
-            Mat[i][j]=lleno;
-        }
-    }
+void imprimecabezas(int N,float **b);
+
+void imprime(int N,float **b);
+
+int main(){
+
+cout<<"Escriba un numero del 3 al 11"<<endl;
+
+int N=0;
+
+cin>>N;
+
+if(N<3 || N>11){
+    
+    cout<<"El numero no esta en el rango"<<endl;
 }
-    
-int ImprimeCabezas(){
-       
-    
-    
-}    
-    
-
-int main(void){
-    
-    std::cout<<"Escriba un numero del 3 al 11"<<std::endl;
-    std::cin>>N;
-    if(N<3 || N>11){
-        
-        std::cout<<"El numero esta afuera del rango"<<std::endl;
-    }
-    
     else{
         
-         std::cout<<"Numero: "<<" "<<N<<" "<<"Matriz: "<<" "<< Relleno <<std::endl;
-    }
-   
-    return 0;
+float ** matriz =new float *[N];
+
+for (int i =0; i < N; ++i){
+    
+    matriz[i] =new float[N+2];
+}
+
+rellenos(N,matriz);
+
+imprimecabezas(N,matriz);
+
+}
+
+return 
+0;
+
+}
+
+
+void rellenos(int N,float **b){
+
+
+
+}
+
+void imprimecabezas(int N, float **b){
+
+float sum=0;
+
+for (int i=0; i<N ;i++){
+
+cout<<b[0][i]<<endl;
+
+sum+=b[0][i];
+
+
+
+}
+}
+
+
+
+void imprime(int N,float **b){
+
+for ( int i=0; i < N+2 ;i++) {
+
+for ( int j=0; j < N ;j++) {
+
+cout<<" "<<b[i][j]<<endl;
+
+}
+
+cout<<endl;
+
+}
+
 }
